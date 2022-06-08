fbcca1f28547d5858efea07a865788e470da7af2
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2022-06-08 18:11:01 -0300

    create a Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import "./default.scss";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h2>Hello World!!!</h2>
-      </header>
+      <Header />
+      <h2>Hello World!!!</h2>
     </div>
   );
 }
